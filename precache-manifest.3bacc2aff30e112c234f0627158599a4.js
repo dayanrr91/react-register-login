9a2a08c8dde7a3379311a4fd8e0042430bf4b234
dayanrr91 <dayanrr91@gmail.com>
2020-01-01 21:53:27 -0300

    Updates

---
+++ precache-manifest.3bacc2aff30e112c234f0627158599a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c42e0f25a9d9bf1691f1f1ccfd5362",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "2022bffe92e95c54ff0c",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1d3bdf1caf536757fe23",
+    "url": "/react-deploy/static/js/2.fb45a954.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-deploy/static/js/2.fb45a954.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2022bffe92e95c54ff0c",
+    "url": "/react-deploy/static/js/main.3cc194a2.chunk.js"
+  },
+  {
+    "revision": "12c7d2f2b8243a75cc69",
+    "url": "/react-deploy/static/js/runtime-main.5412bf19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
